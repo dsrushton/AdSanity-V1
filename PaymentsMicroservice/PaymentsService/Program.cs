fbff15f4edb1f768ea80cbb497616beb22f8bc4c
kobeja <84107545+kobeja@users.noreply.github.com>
2024-12-19 08:44:11 -0500

    project init

---
+++ PaymentsMicroservice/PaymentsService/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
