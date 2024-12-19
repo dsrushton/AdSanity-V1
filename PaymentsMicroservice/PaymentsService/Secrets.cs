f273ca5d82141988068d218e82e8f5b768e10ae2
kobeja <84107545+kobeja@users.noreply.github.com>
2024-12-19 11:40:52 -0500

    Create Secrets.cs

---
+++ PaymentsMicroservice/PaymentsService/Secrets.cs
@@ -0,0 +1,12 @@
+﻿namespace PaymentsService;
+
+public static class Secrets
+{
+    // TODO: add your client id here
+    public const string PayPalClientId =
+        "";
+
+    // TODO: add your client key here
+    public const string PayPalClientKey =
+        "";
+}
